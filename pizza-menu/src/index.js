7f32a4db2eb1c6f4c0ca37c0bfe7b666bec23da0
Bemo7 <benjaminedet7@gmail.com>
2024-06-21 13:51:51 +0100

    Module 1 (Working With Components, Props, and JSX) - React Fragments (without key)

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -76,12 +76,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stove oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
