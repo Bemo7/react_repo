20e36da5fd7b66ac7e9ee84a5de4ee0eca49632d
Bemo7 <benjaminedet7@gmail.com>
2024-07-07 22:22:02 +0100

    Module 12 (Effects and Data Fetching) - How not to fetch data in React

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -50,9 +50,15 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = '41d7e043';
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=dragon`)
+    .then((res) => res.json())
+    .then((data) => setMovies(data.Search));
 
   return (
     <>
